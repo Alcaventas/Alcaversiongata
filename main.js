f1d36660ab2e4a6871595c63e37f6524f2d60b61
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-04-17 00:32:27 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `MichiBot`
+global.authFile = `AlcaBot`
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile)
 const msgRetryCounterMap = (MessageRetryMap) => { }
 const msgRetryCounterCache = new NodeCache()
